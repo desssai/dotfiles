ed0d8f5c32f39ca00ee8aa78d96b735c5feffdf0
Neida Carob <ncarob@student.21-school.ru>
2024-03-14 09:23:46 +0300

    fixes for desktop config

--- hypr/scipts/startup.sh
+++ hypr/scipts/startup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-if [ $(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}') = "closed" ]; then
-  hyprctl keyword monitor "eDP-1, disable"
+if [ -f /proc/acpi/button/lid/LID0/state ]; then
+  if [ $(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}') = "closed" ]; then
+    hyprctl keyword monitor "eDP-1, disable"
+  fi
 fi
 
 gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Light-solid'
-gsettings set org.gnome.desktop.interface icon-theme 'WhiteSur-light'
-gsettings set org.gnome.desktop.interface cursor-theme 'SVG-WhiteSur-Cursor'
+gsettings set org.gnome.desktop.interface icon-theme 'WhiteSur'
+gsettings set org.gnome.desktop.interface cursor-theme 'WhiteSur-cursors'
 
-hyprctl setcursor SVG-WhiteSur-Cursor 24
+hyprctl setcursor 'WhiteSur-cursors' 24
 
 eww open bar
