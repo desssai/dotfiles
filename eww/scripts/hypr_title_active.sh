be893b76031861b63ad13e17a41f5b1e36de74f4
Neida Carob <ncarob@student.21-school.ru>
2024-03-16 16:46:29 +0300

    reworked eww & hyprland messages

--- eww/scripts/hypr_title_active.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-socat -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock |
-while read -r line; do
-  if [[ ${line:0:13} = "activewindow>" ]]; then
-    echo $line | awk -F ">>" '{print $2}' | awk -F "," '{print $1}';
-  fi
-done
