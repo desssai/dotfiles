7057551f4b42f73129e59f4e4e8d97bf2210ba48
Neida Carob <ncarob@student.21-school.ru>
2024-03-24 01:57:09 +0300

    changed variables for bluetooth-button in eww

--- eww/scripts/bluetooth_scan.sh
+++ eww/scripts/bluetooth_scan.sh
@@ -1,15 +1,7 @@
-#!/bin/expect -f
+#!/bin/sh
 
-spawn bluetoothctl
-send "scan on\r"
+bluetoothctl devices | cut -f2 -d' ' | 
+  while read uuid;
+    do bluetoothctl info $uuid | jq '.'
+  done
 
-# info (){
-# 	power=$(bluetoothctl show | grep Powered | awk '{print $2}' | sed 's/yes/on/g; s/no/off/g')
-# 	count=$(bluetoothctl devices Connected | wc -l)
-# 	echo "{\"power\": \"${power}\", \"count\": \"${count}\"}"
-# }
-
-# info
-# dbus-monitor --system "sender=:1.3" | while read -r line; do
-# 	info
-# done
