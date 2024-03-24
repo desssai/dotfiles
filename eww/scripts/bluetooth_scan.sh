1839d0d23156f111ed6e2d2fdab51632232fd299
Neida Carob <ncarob@student.21-school.ru>
2024-03-24 23:31:58 +0300

    removed shadow windows & stylized bluetooth window widget

--- eww/scripts/bluetooth_scan.sh
+++ eww/scripts/bluetooth_scan.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-bluetoothctl devices | cut -f2 -d' ' | 
-  while read uuid;
-    do bluetoothctl info $uuid | jq '.'
-  done
+devices=($(bluetoothctl devices | cut -f2 -d' '))
 
+for i in "${!devices[@]}"; do
+  text+=$(bluetoothctl info ${devices[i]} | grep 'Name\|Connected' | sed 's/.//' | yq)
+done
+
+echo $text | jq 
+# bluetoothctl devices | cut -f2 -d' ' | 
+#   while read uuid; do
+#     devices[i]="$(bluetoothctl info $uuid | grep 'Name\|Connected' | sed 's/.//; s/Connected/\nConnected/')"
+#     i=$((i+1))
+#   done
+
+# echo "DP array size is %d\n" "${#devices[@]}"
+# echo "%s " "${devices[@]}"
+# echo "\n"
+# echo ${devices[*]}
