d478d6686cff285874eb0af04d6b03064db90a2c
Neida Carob <ncarob@student.21-school.ru>
2024-04-17 10:50:44 +0300

    feat: mapping for diagnostic buffer navigantion via <c-j> and <c-k>. removed unnecessary mappings for vscode-like line movements.

--- nvim/lua/mappings/general.lua
+++ nvim/lua/mappings/general.lua
@@ -5,12 +5,12 @@ local mappings = {
 		["<C-a>"] = { "<ESC>^i", "Beginning of line" },
 		["<C-e>"] = { "<End>", "End of line" },
 
+		-- Cycle diagnostic buffers
+		["<C-j>"] = { "<cmd> cnext <CR>", "Next diagnostics buffer" },
+		["<C-k>"] = { "<cmd> cprev <CR>", "Previous diagnostics buffer" },
+
 		-- Leave insert mode
 		["jk"] = { "<ESC>", "Leave insert mode" },
-
-		-- Lines Movement
-		["<∆>"] = { "<Esc>:m .+1<CR>==gi", "Move the current line down" },
-		["<˚>"] = { "<Esc>:m .+1<CR>==gi", "Move the current line up" },
 	},
 
 	n = {
@@ -22,7 +22,11 @@ local mappings = {
 
 		-- Go to  beginning and end
 		["<C-a>"] = { "<ESC>^i", "Beginning of line" },
-		["<C-e>"] = { "<End>i", "End of line" },
+		["<C-e>"] = { "<End>a", "End of line" },
+
+		-- Cycle diagnostic buffers
+		["<C-j>"] = { "<cmd> cnext <CR>", "Next diagnostics buffer" },
+		["<C-k>"] = { "<cmd> cprev <CR>", "Previous diagnostics buffer" },
 
 		-- Switch Between Windows
 		["<leader>h"] = { "<C-w>h", "Window left" },
@@ -46,17 +50,9 @@ local mappings = {
 		["<leader>v"] = { "<C-w>v", "Split windows vertically" },
 		["<leader>x"] = { "<C-w>s", "Split windows horizontally" },
 		["<leader>b"] = { "<C-w>=", "Balance open windows" },
-
-		-- Lines Movement
-		["∆"] = { ":m .+1<CR>==", "Move the current line down" },
-		["˚"] = { ":m .-2<CR>==", "Move the current line up" },
 	},
 
 	v = {
-		-- Lines Movement
-		["∆"] = { ":m '>+1<CR>gv=gv", "Move selected lines down" },
-		["˚"] = { ":m '<-2<CR>gv=gv", "Move selected lines up" },
-
 		-- Rename word under cursor
 		["<leader>rn"] = {
 			":s/<C-r><C-w>//g<Left><Left>",
