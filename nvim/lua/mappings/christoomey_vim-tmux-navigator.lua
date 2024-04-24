26e4be9eead7f0e924e54d87bf3cb45f4fbf9599
Neida Carob <ncarob@student.21-school.ru>
2024-04-24 11:52:43 +0300

    fix: resolved tmux mapping being overwritten, restored dap-ui mappings.

--- nvim/lua/mappings/christoomey_vim-tmux-navigator.lua
+++ nvim/lua/mappings/christoomey_vim-tmux-navigator.lua
@@ -15,10 +15,10 @@ local mappings = {
 	},
 
 	v = {
-		["<m-h>"] = { "<cmd><C-U>TmuxNavigateLeft<cr>", "Window left" },
-		["<m-j>"] = { "<cmd><C-U>TmuxNavigateDown<cr>", "Window right" },
-		["<m-k>"] = { "<cmd><C-U>TmuxNavigateUp<cr>", "Window down" },
-		["<m-l>"] = { "<cmd><C-U>TmuxNavigateRight<cr>", "Window up" },
+		["<m-h>"] = { "<cmd>TmuxNavigateLeft<cr>", "Window left" },
+		["<m-j>"] = { "<cmd>TmuxNavigateDown<cr>", "Window right" },
+		["<m-k>"] = { "<cmd>TmuxNavigateUp<cr>", "Window down" },
+		["<m-l>"] = { "<cmd>TmuxNavigateRight<cr>", "Window up" },
 	},
 }
 
--- nvim/lua/mappings/rcarriga_nvim-dap-ui.lua
+++ nvim/lua/mappings/rcarriga_nvim-dap-ui.lua
@@ -1,24 +1,19 @@
 local mappings = {
-
-	i = {
-		["<m-h>"] = { "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		["<m-j>"] = { "<cmd><C-U>TmuxNavigateDown<cr>" },
-		["<m-k>"] = { "<cmd><C-U>TmuxNavigateUp<cr>" },
-		["<m-l>"] = { "<cmd><C-U>TmuxNavigateRight<cr>" },
-	},
-
 	n = {
-		["<m-h>"] = { "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		["<m-j>"] = { "<cmd><C-U>TmuxNavigateDown<cr>" },
-		["<m-k>"] = { "<cmd><C-U>TmuxNavigateUp<cr>" },
-		["<m-l>"] = { "<cmd><C-U>TmuxNavigateRight<cr>" },
-	},
-
-	v = {
-		["<m-h>"] = { "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		["<m-j>"] = { "<cmd><C-U>TmuxNavigateDown<cr>" },
-		["<m-k>"] = { "<cmd><C-U>TmuxNavigateUp<cr>" },
-		["<m-l>"] = { "<cmd><C-U>TmuxNavigateRight<cr>" },
+		["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>", "Toggle a debug breakpoint" },
+		["<leader>ds"] = { "<cmd> DapContinue <CR>", "Start a debug session" },
+		["<leader>dl"] = { "<cmd> DapStepInto <CR>", "Dap Step Into" },
+		["<leader>dj"] = { "<cmd> DapStepOver <CR>", "Dap Step Over" },
+		["<leader>dk"] = { "<cmd> DapStepOut <CR>", "Dap Step Out" },
+		["<leader>dq"] = { "<cmd> DapTerminate <CR>", "Terminate the debug session" },
+		["<leader>dd"] = {
+			function()
+				local widgets = require("dap.ui.widgets")
+				local sidebar = widgets.sidebar(widgets.scopes)
+				sidebar.open()
+			end,
+			"Open debug sidebar",
+		},
 	},
 }
 
