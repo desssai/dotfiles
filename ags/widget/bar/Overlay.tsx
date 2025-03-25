241f0c02eb8102e7cb39cd508b93c7f2f9928375
Neida Carob <ncarob@student.21-school.ru>
2025-03-25 23:10:00 +0300

    wip: ags v2 migration-0.1

---
+++ ags/widget/bar/Overlay.tsx
@@ -0,0 +1,20 @@
+import { App, Astal, Gtk, Gdk, hook } from "astal/gtk4";
+
+export default function BarOverlay(gdkmonitor: Gdk.Monitor) {
+  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
+
+  return (
+    <window
+      visible
+      name={`overlay`}
+      gdkmonitor={gdkmonitor}
+      namespace={"hello"}
+      exclusivity={Astal.Exclusivity.IGNORE}
+      layer={Astal.Layer.OVERLAY}
+      anchor={TOP | LEFT | RIGHT}
+      application={App}
+      defaultHeight={10}
+      onHoverEnter={() => App.toggle_window("bar")}
+    ></window>
+  );
+}
