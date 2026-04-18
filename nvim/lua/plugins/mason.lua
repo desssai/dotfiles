e14b660b53cc2dbdff41b5d4ab3af1d1fd01378e
Neida Carob <ncarob@student.21-school.ru>
2026-04-18 18:56:09 +0300

    fix: formatted mason plugin config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,69 +1,69 @@
 local plugin = {
-	src = "https://github.com/mason-org/mason.nvim",
+	src = 'https://github.com/mason-org/mason.nvim',
 	cmd = {
-		"Mason",
-		"MasonInstall",
-		"MasonInstallAll",
-		"MasonUninstall",
-		"MasonUninstallAll",
-		"MasonLog",
+		'Mason',
+		'MasonInstall',
+		'MasonInstallAll',
+		'MasonUninstall',
+		'MasonUninstallAll',
+		'MasonLog',
 	},
 	setup = function()
 		local ensure_installed = {
 			-- LSP SERVERS
-			"bash-language-server",         -- Bash LSP
-			"clangd",                       -- C, C++ LSP
+			'bash-language-server', -- Bash LSP
+			'clangd', -- C, C++ LSP
 			-- "cmake-language-server", -- CMake LSP
-			"docker-language-server",   -- Docker & Compose LSP
-			"gopls",                        -- Go LSP
-			"lua-language-server",          -- Lua LSP
-			"marksman",                     -- Markdown LSP
-			"css-lsp",                      -- CSS LSP
-			"typescript-language-server",   -- TS & JS LSP
-			"templ",                        -- Golang HTML Templates LSP
-			"python-lsp-server",            -- Python LSP
-			"sqlls",                        -- SQL LSP
-			"tailwindcss-language-server",  -- Tailwindcss LSP
-			"html-lsp",                     -- HTML LSP
+			'docker-language-server', -- Docker & Compose LSP
+			'gopls', -- Go LSP
+			'lua-language-server', -- Lua LSP
+			'marksman', -- Markdown LSP
+			'css-lsp', -- CSS LSP
+			'typescript-language-server', -- TS & JS LSP
+			'templ', -- Golang HTML Templates LSP
+			'python-lsp-server', -- Python LSP
+			'sqlls', -- SQL LSP
+			'tailwindcss-language-server', -- Tailwindcss LSP
+			'html-lsp', -- HTML LSP
 
 			-- LINTING
-			"luacheck",
+			'luacheck',
 
 			-- FORMATTING
-			"clang-format", -- C, C++ Formatting
-			"stylua",    -- Lua Formatting
-			"gofumpt",   -- Go Formatting
-			"golines",   -- Go lines Formatting
-			"goimports", -- Go imports Formatting
-			"prettier",  -- HTML, CSS, JS Formatting
-			"sqlfmt",    -- SQL Formatting
-			"black",     -- Python Formatting
+			'clang-format', -- C, C++ Formatting
+			'stylua', -- Lua Formatting
+			'gofumpt', -- Go Formatting
+			'golines', -- Go lines Formatting
+			'goimports', -- Go imports Formatting
+			'prettier', -- HTML, CSS, JS Formatting
+			'sqlfmt', -- SQL Formatting
+			'black', -- Python Formatting
 
 			-- DEBUGGING
-			"delve", -- Go Debug server
+			'delve', -- Go Debug server
 			-- "codelldb", -- C, C++ Debugger
 		}
 
-		vim.api.nvim_create_user_command("MasonInstallAll", function()
-			vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
+		vim.api.nvim_create_user_command('MasonInstallAll', function()
+			vim.cmd('MasonInstall ' .. table.concat(ensure_installed, ' '))
 		end, {})
 		require('mason').setup({
 			ui = {
 				icons = {
-					package_pending = " ",
-					package_installed = " ",
-					package_uninstalled = " ",
+					package_pending = ' ',
+					package_installed = ' ',
+					package_uninstalled = ' ',
 				},
 
 				keymaps = {
-					toggle_server_expand = "<CR>",
-					install_server = "i",
-					update_server = "u",
-					check_server_version = "v",
-					update_all_servers = "U",
-					check_outdated_servers = "C",
-					uninstall_server = "D",
-					cancel_installation = "<C-c>",
+					toggle_server_expand = '<CR>',
+					install_server = 'i',
+					update_server = 'u',
+					check_server_version = 'v',
+					update_all_servers = 'U',
+					check_outdated_servers = 'C',
+					uninstall_server = 'D',
+					cancel_installation = '<C-c>',
 				},
 			},
 			max_concurrent_installers = 10,
