8b819cd07df51769f7a01e4e4e9c590d6d75db93
Neida Carob <ncarob@student.21-school.ru>
2026-07-07 16:08:53 +0300

    sync: archwork

--- hypr/scipts/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f /proc/acpi/button/lid/LID0/state ]; then
-  if [ $(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}') = "closed" ]; then
-    hyprctl keyword monitor "eDP-1, disable"
-  fi
-fi
